36dab0a34c354a4a7d2497eec17bb633da4a02e3
Arthur Burgan <arthur@Arthurs-MacBook-Air.local>
2019-02-11 10:13:49 +0100

    done

--- starter-code/src/main.js
+++ starter-code/src/main.js
@@ -25,7 +25,13 @@ var cards = [
   { name: 'thor',            img: 'thor.jpg' }
 ];
 
+var clickedCards = [];
+var clickedTurn = [];
+var cardOne; var cardTwo;
+
 $(document).ready(function(){
+  var clickedCount = $('#pairs_clicked');
+  var guessedCount = $('#pairs_guessed');
   var memoryGame = new MemoryGame(cards);
   var html = '';
   memoryGame.cards.forEach(function (pic) {
@@ -41,6 +47,20 @@ $(document).ready(function(){
   // Bind the click event of each element to a function
   $('.back').click(function () {
     // TODO: write some code here
+    //Push Names of Cards only(for comparison's sake) and push the whole element to target its attributes
+    if(clickedCards.length < 2) {clickedCards.push($(this).parent().attr('data-card-name'));clickedTurn.push($(this).parent());}
+    else {clickedCards = []; clickedCards.push($(this).parent().attr('data-card-name'));clickedTurn = [];clickedTurn.push($(this).parent())}
+    $(this).parent().children().toggleClass('back');
+    $(this).parent().children().toggleClass('front');
+
+    if (clickedCards.length == 2) {
+      if(memoryGame.checkIfPair(clickedCards[0],clickedCards[1]) == true){memoryGame.pairsClicked+=1;memoryGame.pairsGuessed+=1;}
+      else{memoryGame.pairsClicked+=1;setTimeout(function() {memoryGame.turnBack(clickedTurn)}, 500);}
+    }//else{memoryGame.turnBack(clickedTurn)}
+    // memoryGame.turnBack(clickedTurn);
+    clickedCount.text(memoryGame.pairsClicked);
+    guessedCount.text(memoryGame.pairsGuessed);
+    memoryGame.isFinished();
   });
 });
 
--- starter-code/src/memory.js
+++ starter-code/src/memory.js
@@ -1,12 +1,31 @@
-var MemoryGame = function (cards) {
-  this.cards = cards;
-};
-
-MemoryGame.prototype.shuffleCards = function () {
-};
-
-MemoryGame.prototype.checkIfPair = function (firstCard, secondCard) {
+class MemoryGame {
+  constructor(cards) {
+    this.cards = cards;
+    this.pickedCards = [];
+    this.pairsClicked = 0;
+    this.pairsGuessed = 0;
+  }
+  shuffleCards(cards) {
+    for (let i = 0; i < cards.length-1; i++) {
+      let j = (Math.floor(Math.random() * this.cards.length));
+      let k = cards[i];
+      cards[i] = cards[j];
+      cards[k] = cards[i];
+    }
+  }
+  checkIfPair(firstCard, secondCard) {
+    return firstCard == secondCard;
+    // if (firstCard == secondCard) {this.pairsClicked+=1; this.pairsGuessed+=1; return true}
+    // else{this.pairsClicked+=1; return false;}
+  }
+  isFinished() {
+    if (this.pairsGuessed == this.cards.length / 2) {alert(`Congratulations! You've won! GG!`)}
+  }
+  turnBack(array) {
+    for (let i = 0; i < array.length; i++){
+      $(array[i]).children().toggleClass('front');
+      $(array[i]).children().toggleClass('back');
+  }
+  }
 }
 
-MemoryGame.prototype.isFinished = function () {
-};
